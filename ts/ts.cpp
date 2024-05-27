04543ff1341ea734eea2dfbb505a41ccd2268f14
kth9245 <82037797+kth9245@users.noreply.github.com>
2024-05-27 15:58:09 +0900

    Add file

--- ts/ts.cpp
+++ ts/ts.cpp
@@ -81,18 +81,18 @@ void recvThread(int sd) {
 				myerror(" ");
 				break;
 			}
-		}
-		if (param.broadcast){
-			for (int i=0; i<clients.size();i++){
-				if (clients[i] != sd || param.echo == false){
-					res = ::send(clients[i], buf, res, 0);
-					if (res == 0 || res == -1) {
-						fprintf(stderr, "send return %zd", res);
-						myerror(" ");
-						break;
+			if (param.broadcast){
+				for (int i=0; i<clients.size();i++){
+					if (clients[i] != sd || param.echo == false){
+						res = ::send(clients[i], buf, res, 0);
+						if (res == 0 || res == -1) {
+							fprintf(stderr, "send return %zd", res);
+							myerror(" ");
+							break;
+						}
 					}
-				}
-			}	
+				}	
+			}
 		}
 	}
 	printf("disconnected\n");
